428fc676cd6e2b244c1b142690ac101e43623393
afeiship <1290657123@qq.com>
2022-02-25 14:08:43 +0800

    feat: update .tmp test dir

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -3,14 +3,17 @@ import fs from 'fs';
 
 describe('api.basic', () => {
   test('normail single value case', (done) => {
-    fn([
-      'https://fasimi.com/',
-      'https://fasimi.com/filters/series.html',
-      'https://fasimi.com/filters/movie.html'
-    ]).then((res) => {
-      expect(fs.existsSync('dist/index.html')).toBe(true);
-      expect(fs.existsSync('dist/filters/series.html')).toBe(true);
-      expect(fs.existsSync('dist/filters/movie.html')).toBe(true);
+    fn(
+      [
+        'https://fasimi.com/',
+        'https://fasimi.com/filters/series.html',
+        'https://fasimi.com/filters/movie.html'
+      ],
+      { dst: '.tmp' }
+    ).then((res) => {
+      expect(fs.existsSync('.tmp/index.html')).toBe(true);
+      expect(fs.existsSync('.tmp/filters/series.html')).toBe(true);
+      expect(fs.existsSync('.tmp/filters/movie.html')).toBe(true);
       done();
     });
   });
